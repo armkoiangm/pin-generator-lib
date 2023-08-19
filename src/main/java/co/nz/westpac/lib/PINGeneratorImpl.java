e5c06b040cc7f0f8db29e01a37c4d48c311e396c
Armen Mkoian <armkoian@gmail.com>
2023-08-20 11:04:24 +1200

    developed PINGenerator implementation

--- src/main/java/co/nz/westpac/lib/PINGeneratorImpl.java
+++ src/main/java/co/nz/westpac/lib/PINGeneratorImpl.java
@@ -1,13 +1,38 @@
 package co.nz.westpac.lib;
 
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
-public class PINGeneratorImpl implements PINGenerator{
+class PINGeneratorImpl implements PINGenerator {
+    private final int UPPER_BOUND = 10000;
+    private final int LOWER_BOUND = 0;
+    private final int PINS_POSSIBLE_MAX_COUNT = 10000;
+
+    private Random randNumGen;
+    private Set<String> pinSet;
+
+    public PINGeneratorImpl() {
+        randNumGen = new Random();
+        pinSet = new HashSet<String>();
+    }
 
     @Override
     public Set<String> GeneratePINs(Integer pinCount) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'GeneratePINs'");
+        if (pinCount < 1) {
+            return pinSet;
+        }
+
+        while (pinSet.size() < pinCount) {
+            int randomDigit = randNumGen.nextInt(LOWER_BOUND, UPPER_BOUND);
+            String randomPin = String.format("%04d", randomDigit);
+            pinSet.add(randomPin);
+
+            if (pinSet.size() == PINS_POSSIBLE_MAX_COUNT) {
+                break;
+            }
+        }
+
+        return pinSet;
     }
-    
 }
