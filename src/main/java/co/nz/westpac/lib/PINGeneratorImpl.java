82a27f37a41e2dd172aca50b0dcfd568b631bbe1
Armen Mkoian <armkoian@gmail.com>
2023-08-20 10:42:18 +1200

    added  PINGenerator implementer class

---
+++ src/main/java/co/nz/westpac/lib/PINGeneratorImpl.java
@@ -0,0 +1,13 @@
+package co.nz.westpac.lib;
+
+import java.util.Set;
+
+public class PINGeneratorImpl implements PINGenerator{
+
+    @Override
+    public Set<String> GeneratePINs(Integer pinCount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'GeneratePINs'");
+    }
+    
+}
